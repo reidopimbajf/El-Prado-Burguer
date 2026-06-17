c0bb76c139effd12a2d1d8752f0ebc2fb09ad4e5
reidopimbajf <joaquim.prado420@gmail.com>
2026-06-17 13:12:54 -0300

    Atualizar o storage.js

--- storage.js
+++ storage.js
@@ -1,1 +1,71 @@
-.
+// =========================
+// STORAGE EL PRADO BURGUER
+// =========================
+
+// Salvar carrinho
+function salvarCarrinho(carrinho) {
+
+    localStorage.setItem(
+        "carrinho",
+        JSON.stringify(carrinho)
+    );
+
+}
+
+// Carregar carrinho
+function carregarCarrinhoStorage() {
+
+    return JSON.parse(
+        localStorage.getItem("carrinho")
+    ) || [];
+
+}
+
+// Limpar carrinho
+function limparCarrinhoStorage() {
+
+    localStorage.removeItem(
+        "carrinho"
+    );
+
+}
+
+// Salvar cupom
+function salvarCupom(cupom) {
+
+    localStorage.setItem(
+        "cupom",
+        cupom
+    );
+
+}
+
+// Carregar cupom
+function carregarCupom() {
+
+    return localStorage.getItem(
+        "cupom"
+    ) || "";
+
+}
+
+// Salvar dados cliente
+function salvarCliente(cliente) {
+
+    localStorage.setItem(
+        "cliente",
+        JSON.stringify(cliente)
+    );
+
+}
+
+// Carregar dados cliente
+function carregarCliente() {
+
+    return JSON.parse(
+        localStorage.getItem(
+            "cliente"
+        )
+    ) || {};
+
+}
